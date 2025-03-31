b3b3b9c166227249985197f72a6fa2993b484bd7
francisA88 <githubemail61@gmail.com>
2025-03-31 13:50:51 +0100

    added env file

--- facerecservice/settings.py
+++ facerecservice/settings.py
@@ -1,28 +1,28 @@
 import os
 from pathlib import Path
-
+import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv("./sk.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = "IUDHIUEHIWUED"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [".pythonanywhere.com", "*.onrender.com", "localhost", "127.0.0.1"]
 
 APPEND_SLASH = False
 
 # Application definition
 
 INSTALLED_APPS = [
-    "daphne",
+    #"daphne",
     "mainapp",
     "django.contrib.admin",
     "django.contrib.auth",
