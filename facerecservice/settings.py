820e4035c139d1b60316ce2391f2ae2c88c80341
francisA88 <githubemail61@gmail.com>
2025-03-31 13:52:40 +0100

    updated settings.py

--- facerecservice/settings.py
+++ facerecservice/settings.py
@@ -1,10 +1,11 @@
 import os
 from pathlib import Path
 import dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv("./sk.env")
+dotenv.load_dotenv("./sk.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
